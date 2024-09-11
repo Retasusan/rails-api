dc10aed5ef9d201b3542daa6eb7120b9d8e1611c
Kenta Goto <kenta9736@gmail.com>
2024-09-12 02:14:56 +0900

    URL修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://job-site-xi.vercel.app/", "https://job-site-xi.vercel.app/post"
+    origins "https://job-site-xi.vercel.app"
 
     resource "*",
       headers: :any,
