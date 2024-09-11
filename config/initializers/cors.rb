3e3a1804e2348fd3d864703b8c78f3549a07960f
Kenta Goto <kenta9736@gmail.com>
2024-09-12 02:46:19 +0900

    urlの変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://job-site-xi.vercel.app"
+    origins "https://job-site-xi.vercel.app/api/v1/posts"
 
     resource "*",
       headers: :any,
